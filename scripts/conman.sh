0e402b26de11d77d4c5736316b68d044717891a5
mbcladwell <mbcladwell@gmail.com>
2025-01-22 07:06:19 -0500

    autocommit

--- scripts/conman.sh
+++ scripts/conman.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 export LC_ALL="C"
-export GUILE_LOAD_PATH=guileloadpath
-export GUILE_LOAD_COMPILED_PATH=guileloadcompiledpath
-#cd $1
-guileexecutable -e '(conmanv4)' -s conmanstorepath/share/guile/site/3.0/conmanv4.scm $1
+##export GUILE_LOAD_PATH=guileloadpath
+##export GUILE_LOAD_COMPILED_PATH=guileloadcompiledpath
+guileexecutable -e '(conmanv4)' -s conmanstorepath/share/guile/site/3.0/conmanv4.scm
 
